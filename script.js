f759420766909611e39e7a7e7d94668693d1cdec
Vincent Chan <vincent.chan@biola.edu>
2017-08-16 14:58:44 -0700

    random dice value

--- script.js
+++ script.js
@@ -26,7 +26,20 @@ const dice = ['aaafrs',
 'nootuw',
 'ooottu']
 
-function shuffle(diceArray) {
+function shake(dice){
+  const shuffled = shuffleDice(dice);
+  shuffled.forEach(dice => {
+    console.log(dice);
+    const letter = rollDice(dice);
+    console.log(letter);
+  })
+}
+
+function rollDice(dice) {
+  return dice[Math.floor(Math.random() * dice.length)]
+}
+
+function shuffleDice(diceArray) {
   for (let i = dice.length; i ; i--) {
     let j = Math.floor(Math.random()*i);
     [diceArray[i-1], diceArray[j]] = [diceArray[j], diceArray[i-1]]
@@ -34,4 +47,4 @@ function shuffle(diceArray) {
   return diceArray;
 }
 
-console.log(shuffle(dice));
+shake(dice);
